ccca1f14e6906c21b4ad8029b61f5239e586776d
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-06 15:17:16 -0300

    feat: create cross icon component

---
+++ src/components/icons/CrossCircleIcon/CrossCircleIcon.jsx
@@ -0,0 +1,13 @@
+const CrossCircleIcon = props => {
+  return (
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" {...props}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
+  )
+}
+
+export default CrossCircleIcon
