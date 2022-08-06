930ddaddacf654828a19c33da7fb40ab1f66b652
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-06 15:16:30 -0300

    feat: create user status component

---
+++ src/components/UserStatus/UserStatus.jsx
@@ -0,0 +1,20 @@
+import CheckCircleIcon from "../icons/CheckCircleIcon/CheckCircleIcon"
+import CrossCircleIcon from "../icons/CrossCircleIcon"
+
+import style from "./UserStatus.module.css"
+
+const UserStatus = ({ status }) => {
+  const classStatus = status ? style.active : style.inactive
+  const [statusText, Icon] = status
+    ? ["Activo", CheckCircleIcon]
+    : ["Inactivo", CrossCircleIcon]
+
+  return (
+    <div className={classStatus}>
+      <Icon className={style.icon} />
+      <span>{statusText}</span>
+    </div>
+  )
+}
+
+export default UserStatus
