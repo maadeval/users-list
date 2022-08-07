9b1d33b3c4e5c155a26e053f45c61f4817f35057
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-07 00:05:39 -0300

    feat: create check tic icon

---
+++ src/components/icons/CheckTicIcon/CheckTicIcon.jsx
@@ -0,0 +1,13 @@
+const CheckTicIcon = props => {
+  return (
+    <svg {...props} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5 13l4 4L19 7"></path>
+    </svg>
+  )
+}
+
+export default CheckTicIcon
