f23c35fc6f443d56b3ebac1b47306820d0c4b95b
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-07 16:04:49 -0300

    feat: apply user list

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,20 +1,28 @@
 import { useFilters } from "lib/hooks/useFilters"
+import { useState } from "react"
 import {
   filterActiveUsers,
   filterUsersByName,
+  paginationUsers,
   sortUsers,
 } from "../../lib/users/filterUsers"
 import UsersListFilters from "../UsersListFilters/UsersListFilters"
+import UsersListPagination from "../UsersListPagination"
 import UsersListRows from "../UsersListRows"
 
 import style from "./UserList.module.css"
 
 const UsersList = ({ initialUsers }) => {
+  const [page, setPage] = useState(1)
+  const [usersPerPage, setUsersPerPage] = useState(3)
   const { search, active, sort, ...setFiltersFunctions } = useFilters()
-
-  let filteredUsers = filterActiveUsers(initialUsers, active)
-  filteredUsers = filterUsersByName(filteredUsers, search)
-  filteredUsers = sortUsers(filteredUsers, sort)
+  const filteredUsers = getUsers(initialUsers, {
+    search,
+    active,
+    sort,
+    page,
+    usersPerPage,
+  })
 
   return (
     <section className={style.layout}>
@@ -26,8 +34,26 @@ const UsersList = ({ initialUsers }) => {
         {...setFiltersFunctions}
       />
       <UsersListRows users={filteredUsers} />
+      <UsersListPagination
+        page={page}
+        usersPerPage={usersPerPage}
+        setPage={setPage}
+        setUsersPerPage={setUsersPerPage}
+      />
     </section>
   )
+}
+
+const getUsers = (
+  initialUsers,
+  { search, active, sort, page = 1, usersPerPage = 3 }
+) => {
+  let filteredUsers = filterActiveUsers(initialUsers, active)
+  filteredUsers = filterUsersByName(filteredUsers, search)
+  filteredUsers = sortUsers(filteredUsers, sort)
+  filteredUsers = paginationUsers(filteredUsers, page, usersPerPage)
+
+  return filteredUsers
 }
 
 export default UsersList
