26af228861c3f9fa2f4c64990416588b8e6c6a5e
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-07 00:03:52 -0300

    feat: add arrow down icon

---
+++ src/components/icons/ArrowDownIcon/ArrowDownIcon.jsx
@@ -0,0 +1,13 @@
+const ArrowDownIcon = props => {
+  return (
+    <svg {...props} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 9l-7 7-7-7"></path>
+    </svg>
+  )
+}
+
+export default ArrowDownIcon
