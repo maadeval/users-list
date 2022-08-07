e34c3ee2310cf7519b678511a41e149ac074ea54
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-07 14:33:13 -0300

    ref: remove magic values

--- src/constants/users-list.js
+++ src/constants/users-list.js
@@ -1,26 +1,28 @@
+import { ROLE_OPTIONS } from "./sort-users-select"
+
 export const USERS_LIST = [
   {
     username: "madeval",
     name: "Mateo Alvarez",
-    role: "student",
+    role: ROLE_OPTIONS.STUDENT,
     active: false,
   },
   {
     username: "midudev",
     name: "Miguel Angel Duran",
-    role: "teacher",
+    role: ROLE_OPTIONS.TEACHER,
     active: true,
   },
   {
     username: "Pheralb",
     name: "Pablo",
-    role: "other",
+    role: ROLE_OPTIONS.OTHER,
     active: false,
   },
   {
     username: "davidponce",
     name: "David Ponce",
-    role: "student",
+    role: ROLE_OPTIONS.STUDENT,
     active: true,
   },
 ]
