44ffb6c5c855a8a15e9c799efa11c16b1208c2d2
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-08 22:45:30 -0300

    feat: create page selector

---
+++ src/components/PageSelector/PageSelector.jsx
@@ -0,0 +1,33 @@
+import { PAGE_VALUES } from "../../constants/page-selectors"
+import ButtonIcon from "../ButtonIcon"
+import ArrowLeft from "../icons/ArrowLeft"
+import ArrowRight from "../icons/ArrowRight"
+
+import style from "./PageSelector.module.css"
+
+const PageSelector = ({ currentPage, totalPages, setPage }) => {
+  const isFirstPage = currentPage === PAGE_VALUES.PAGE
+  const isLastPage = currentPage === totalPages || totalPages === 0
+  console.log({ currentPage, totalPages, setPage })
+  return (
+    <div className={style.wrapper}>
+      <ButtonIcon
+        disabled={isFirstPage}
+        icon={ArrowLeft}
+        isFill
+        onClick={() => setPage(currentPage - 1)}
+      />
+      <p>
+        Página {currentPage} de {totalPages || 1}
+      </p>
+      <ButtonIcon
+        disabled={isLastPage}
+        icon={ArrowRight}
+        isFill
+        onClick={() => setPage(currentPage + 1)}
+      />
+    </div>
+  )
+}
+
+export default PageSelector
