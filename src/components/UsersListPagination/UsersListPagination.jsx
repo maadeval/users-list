f63795f4ea22f048c42cc5f063cf35adb168d0f2
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-09 00:18:57 -0300

    ref: change de numbers of group users page

--- src/components/UsersListPagination/UsersListPagination.jsx
+++ src/components/UsersListPagination/UsersListPagination.jsx
@@ -16,12 +16,9 @@ const UsersListPagination = ({
         <Select
           value={usersPerPage}
           onChange={e => setUsersPerPage(Number(e.target.value))}>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
           <option value={4}>4</option>
-          <option value={5}>5</option>
           <option value={6}>6</option>
+          <option value={8}>8</option>
         </Select>
         <label>Elementos por pagina</label>
       </div>
