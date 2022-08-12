7faa4883ed596dc71ac38abb1bd441cff43e2eec
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-11 23:48:37 -0300

    feat: create button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import style from "./Button.module.css"
+
+const VARIANT_CLASSES = {
+  primary: style.primary,
+  secondary: style.secondary,
+}
+
+const Button = ({ variant = "primary", children, className, ...props }) => {
+  return (
+    <button
+      {...props}
+      className={`${VARIANT_CLASSES[variant]} ${style.button} ${className}`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
