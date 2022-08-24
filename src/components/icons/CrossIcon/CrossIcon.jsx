430900a92da7a951f232d6099d1a55d39b96a0a9
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-23 22:03:30 -0300

    feat: create cross icon component

---
+++ src/components/icons/CrossIcon/CrossIcon.jsx
@@ -0,0 +1,19 @@
+const CrossIcon = ({ ...props }) => {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 13L13 1M1 1L13 13"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default CrossIcon
