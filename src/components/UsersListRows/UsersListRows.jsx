e720bea79405733483ffc91919c20877794ffc25
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-23 22:05:17 -0300

    ref: add styles

--- src/components/UsersListRows/UsersListRows.jsx
+++ src/components/UsersListRows/UsersListRows.jsx
@@ -1,16 +1,18 @@
 import UserRow from "../UserRow"
 
+import style from "./UsersListRows.module.css"
+
 const UsersListRows = ({ users, error, loading }) => {
   if (loading) return <p>Cargando...</p>
   if (error) return <p>{error}</p>
   if (users.length === 0) return <p>No hay resultados en la busqueda</p>
 
   return (
-    <>
+    <section className={style.wrapper}>
       {users.map(user => (
         <UserRow key={user.id} {...user} />
       ))}
-    </>
+    </section>
   )
 }
 
