e1643645b88860025669b3b60a5e22e7d7f7df42
Mateo Álvarez <alvarez.fing@gmail.com>
2022-08-26 22:44:25 -0300

    feat: create function to submit users an UX

--- src/components/UsersListCreateForm/UsersListCreateForm.jsx
+++ src/components/UsersListCreateForm/UsersListCreateForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { ROLE_OPTIONS } from "../../constants/sortUsersSelect"
 import { useFormValues } from "../../lib/hooks/useFormValues"
 import Button from "../Button/Button"
@@ -11,52 +12,94 @@ import CrossIcon from "../icons/CrossIcon/CrossIcon"
 import style from "./UsersListCreateForm.module.css"
 
 const UsersListCreateForm = ({ onClose }) => {
+  const [isSubmiting, setIsSubmitting] = useState(false)
   const { name, username, setName, setUsername } = useFormValues()
 
-  const onSubmit = e => {
-    e.preventDefault()
-  }
+  const isDisabled =
+    !name.value ||
+    !username.value ||
+    name.error ||
+    username.error ||
+    username.loading ||
+    isSubmiting
 
   return (
-    <form className={style.form} onSubmit={onSubmit}>
-      <div className={style.formRow}>
-        <InputText
-          className={style.input}
-          error={name.error}
-          label="Nombre"
-          onChange={e => setName(e.target.value)}
-          value={name.value}
-        />
-        <InputTextAsync
-          className={style.input}
-          error={username.error}
-          success={username.value && !username.error && !username.loading}
-          loading={username.loading}
-          label="Username"
-          onChange={e => setUsername(e.target.value)}
-          value={username.value}
-        />
-      </div>
-      <div className={style.formRow}>
-        <Select name="role">
-          <option value={ROLE_OPTIONS.TEACHER}>Profesor</option>
-          <option value={ROLE_OPTIONS.STUDENT}>Alumno</option>
-          <option value={ROLE_OPTIONS.OTHER}>Otro</option>
-        </Select>
-        <label className={style.checkbox}>
-          <InputCheckbox name="active" />
-          <span>Activo?</span>
-        </label>
-        <Button type="submit">Editar usuario</Button>
-      </div>
+    <div className={style.wrapper}>
+      <form
+        onSubmit={e =>
+          handleSubmit(e, name, username, setIsSubmitting, onClose)
+        }>
+        <div className={style.formRow}>
+          <InputText
+            className={style.input}
+            error={name.error}
+            label="Nombre"
+            onChange={e => setName(e.target.value)}
+            value={name.value}
+          />
+          <InputTextAsync
+            className={style.input}
+            error={username.error}
+            success={username.value && !username.error && !username.loading}
+            loading={username.loading}
+            label="Username"
+            onChange={e => setUsername(e.target.value)}
+            value={username.value}
+          />
+        </div>
+        <div className={style.formRow}>
+          <Select name="role">
+            <option value={ROLE_OPTIONS.TEACHER}>Profesor</option>
+            <option value={ROLE_OPTIONS.STUDENT}>Alumno</option>
+            <option value={ROLE_OPTIONS.OTHER}>Otro</option>
+          </Select>
+          <label className={style.checkbox}>
+            <InputCheckbox name="active" />
+            <span>Activo?</span>
+          </label>
+          <Button disabled={isDisabled} type="submit">
+            {isSubmiting ? "Creando..." : "Crear usuario"}
+          </Button>
+        </div>
+      </form>
       <ButtonIcon
         className={style.close}
         icon={CrossIcon}
         isFill
         onClick={onClose}
       />
-    </form>
+    </div>
   )
+}
+
+const handleSubmit = async (ev, name, username, setIsSubmitting, onClose) => {
+  ev.preventDefault()
+
+  setIsSubmitting(true)
+
+  const newUser = {
+    id: crypto.randomUUID(),
+    name: name.value,
+    username: username.value,
+    active: ev.target.active.checked,
+    role: ev.target.role.value,
+  }
+
+  try {
+    const res = await fetch("http://localhost:4000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+
+    if (res.ok) return onClose()
+
+    setIsSubmitting(false)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 export default UsersListCreateForm
