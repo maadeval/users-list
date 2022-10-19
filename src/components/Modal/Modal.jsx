f3941172fdd9c6444894d644b6bb618b5a53cb3f
madeval <alvarez.fing@gmail.com>
2022-10-18 22:22:33 -0300

    feat: remove overflow of the body when open modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react"
 import { createPortal } from "react-dom"
 import ButtonIcon from "../ButtonIcon/ButtonIcon"
 import CrossIcon from "../icons/CrossIcon/CrossIcon"
 import style from "./Modal.module.css"
 
 const Modal = ({ closeModal, children }) => {
+  useEffect(() => {
+    if (children) {
+      document.body.classList.add(style.bodyOverflow)
+
+      return () => document.body.classList.remove(style.bodyOverflow)
+    }
+  }, [children])
+
   if (!children) return null
 
   return createPortal(
